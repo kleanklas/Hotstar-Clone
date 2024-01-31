796a7b71a3f65ab11a07071662a27b907ab62b8e
kleanklas <106208166+kleanklas@users.noreply.github.com>
2024-01-30 22:43:00 -0800

    Update provider.tf

--- EKS_TERRAFORM/provider.tf
+++ EKS_TERRAFORM/provider.tf
@@ -10,6 +10,4 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-    access_key = "AKIA4MS3FNUE6TNWFRQ4"
-    secret_key = "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"
 }
